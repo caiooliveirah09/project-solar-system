7ff3190fb36b7af0c50f63975f21bdadc6552a52
caiooliveirah09 <leelros@hotmail.com>
2022-06-24 18:11:42 -0300

    i solved requirement 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class Mission extends React.Component {
   render() {
-    return <div data-testid="missions" />;
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
   }
 }
 
